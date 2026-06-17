3ba9618db9015d17650e0e949a9266e316890eaa
unknown <fauziagustian68@gmail.com>
2026-06-17 12:09:26 +0700

    add thumbnail share

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://selam-ilmu.pages.dev',
+  site: 'https://selam-ilmu.vercel.app',
   vite: {
     plugins: [tailwindcss()]
   },
